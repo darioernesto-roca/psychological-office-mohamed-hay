40eab8ad00977aa7b9804fed477999bbb602f983
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2023-03-12 10:07:04 -0500

    Updating package json and app

--- src/app.js
+++ src/app.js
@@ -1,41 +1,44 @@
 // Setting the server
-const express = require('express');
+const express = require("express");
 const app = express();
-const ejs = require('ejs');
+const ejs = require("ejs");
 const routes = require("../routes/index");
-const expressLayouts = require('express-ejs-layouts');
-const cors = require('cors');
-const path = require('path');
-const flash = require('connect-flash');
-const session = require('express-session');
-const bodyParser = require('body-parser');
-
+const expressLayouts = require("express-ejs-layouts");
+const cors = require("cors");
+const path = require("path");
+const flash = require("connect-flash");
+const session = require("express-session");
+const bodyParser = require("body-parser");
 
 const PORT = 5000;
 
 // Add cors
 app.use(cors());
 
-app.use(session({
-    secret: 'secret',
+app.use(
+  session({
+    secret: "secret",
     resave: true,
-    saveUninitialized: true
-  }));
-  
-  app.use(flash());
+    saveUninitialized: true,
+  })
+);
+
+app.use(flash());
 
 // Add body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Static folder
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, "../public")));
 
 // Set the PORT
-app.listen(PORT, () => console.log(`Server listening in port: localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server listening in port: localhost:${PORT}`)
+);
 
 // Set Template Engine
-app.use(expressLayouts)
-app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.set("view engine", "ejs");
 
 // Set routes
 app.use(routes);
